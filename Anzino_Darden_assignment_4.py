8c7a55999dac36668a0fee066209e7023d342d6e
asdarden-design <asdarden@aggies.ncat.edu>
2025-09-26 20:52:44 -0400

    Anzino_Darden_assignment_4.py

---
+++ Anzino_Darden_assignment_4.py
@@ -0,0 +1,5 @@
+student_name = "Aznino Darden"  # Replace with your actual name
+current_gpa = 3.2  # Replace with float (1.0 to 4.0)
+study_hours = 25   # Integer
+social_points = 50 # Integer
+stress_level = 30  # Integer (0-100)
